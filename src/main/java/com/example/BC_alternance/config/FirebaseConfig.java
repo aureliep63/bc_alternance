ef34e7cc286cfce382cdec8cb3f320a1ed2a6bc5
aureliep63 <aurelie.pedro.r@gmail.com>
2025-08-10 18:11:08 +0200

    firebase

--- src/main/java/com/example/BC_alternance/config/FirebaseConfig.java
+++ src/main/java/com/example/BC_alternance/config/FirebaseConfig.java
@@ -3,25 +3,38 @@ package com.example.BC_alternance.config;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
-import jakarta.annotation.PostConstruct;
 import org.springframework.context.annotation.Configuration;
-
-import java.io.FileInputStream;
+import javax.annotation.PostConstruct;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 
 @Configuration
 public class FirebaseConfig {
 
     @PostConstruct
     public void init() throws IOException {
-        FileInputStream serviceAccount = new FileInputStream("src/main/resources/firebase/firebase-config.json");
+        // Lire la configuration Firebase à partir de la variable d'environnement
+        String firebaseConfigJson = System.getenv("FIREBASE_CONFIG_JSON");
 
-        FirebaseOptions options = FirebaseOptions.builder()
-                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                .build();
+        // Vérifier si la variable d'environnement est définie et non vide
+        if (firebaseConfigJson != null && !firebaseConfigJson.isEmpty()) {
+            // Créer un InputStream à partir de la chaîne JSON
+            InputStream serviceAccount = new ByteArrayInputStream(firebaseConfigJson.getBytes(StandardCharsets.UTF_8));
 
-        if (FirebaseApp.getApps().isEmpty()) {
-            FirebaseApp.initializeApp(options);
+            FirebaseOptions options = FirebaseOptions.builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .build();
+
+            // Initialiser l'application Firebase si elle n'a pas déjà été initialisée
+            if (FirebaseApp.getApps().isEmpty()) {
+                FirebaseApp.initializeApp(options);
+            }
+        } else {
+            // Lancer une exception si la variable d'environnement n'est pas configurée,
+            // ce qui empêchera l'application de démarrer.
+            throw new IOException("La variable d'environnement 'FIREBASE_CONFIG_JSON' n'est pas configurée. L'application ne peut pas démarrer.");
         }
     }
 }
